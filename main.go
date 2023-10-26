a6fb225aedfb47f8890a237fdd2b0d7b51cad142
Ramanuj Om <114493547+Omramanuj@users.noreply.github.com>
2023-10-26 19:27:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"github.com/OmRamanuj/golangMongoCrud/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -21,26 +19,6 @@ func main() {
 	http.ListenAndServe("localhost:8080", r)
 }
 
-func db() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
-	return client
-}
 
 func getSession() *mgo.Session {
 	s, err := mgo.Dial("mongodb://127.0.0.1:27017")
