5361e495ece4586ae22bc51b0b11fbf553f01a26
Omramanuj <ramanuj.om.r@gmail.com>
2023-10-26 19:23:19 +0530

    crud

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+	"context"
+	"fmt"
+	"log"
+	"github.com/OmRamanuj/golangMongoCrud/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func db() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
